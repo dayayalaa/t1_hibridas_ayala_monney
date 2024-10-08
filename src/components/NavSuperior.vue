51afa88c6c7a181f55fc58a7e4e2bac68cfbc9be
dayayalaa <day1414@hotmail.com>
2024-10-08 16:52:13 -0300

    vista

---
+++ src/components/NavSuperior.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav class="bg-gray-800 p-4 flex items-center justify-start">
+      <img src="/img/logoArcana.png" alt="Logo" class="h-10 w-10 mr-4">
+      <span class="text-white text-lg font-semibold">Api arcana</span>
+    </nav>
+  </template>
+  
+  
+
+  
